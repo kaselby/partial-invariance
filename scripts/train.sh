82a338d96b21cdb80d57f2729a173858f2504eaa
Kira Selby <kaselby@uwaterloo.ca>
2021-09-14 12:15:49 -0400

    sbatch and checkpointing

--- scripts/train.sh
+++ scripts/train.sh
@@ -5,7 +5,8 @@
 #SBATCH --ntasks=1
 #SBATCH --gres=gpu:4
 #SBATCH --partition=t4v1,t4v2,p100,rtx6000
-#SBATCH -c 1
+#SBATCH --cpus-per-gpu=1
 #SBATCH --mem=25GB
+#SBATCH --exclude=gpu109
 
-python3 train.py $1 --overwrite
+python3 train.py $1
