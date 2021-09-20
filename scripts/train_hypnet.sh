aa5b5f4e9470a8cfac26f3b6859ea3f5aa11a9a9
Kira Selby <kaselby@uwaterloo.ca>
2021-09-20 11:22:00 -0400

    filter dataset for hypnet

---
+++ scripts/train_hypnet.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --job-name=test
+#SBATCH --output=logs/slurm-%j.txt
+#SBATCH --open-mode=append
+#SBATCH --ntasks=1
+#SBATCH --gres=gpu:1
+#SBATCH --partition=t4v1,t4v2,p100,rtx6000
+#SBATCH --cpus-per-gpu=1
+#SBATCH --mem=25GB
+#SBATCH --exclude=gpu109
+
+python3 train_hypnet.py $1
