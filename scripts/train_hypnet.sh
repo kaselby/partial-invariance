fdcc42515ea1b9e7ee8f56cfcf7445c9dc3a1bc5
Kira Selby <kaselby@uwaterloo.ca>
2021-09-28 11:49:36 -0400

    eval

--- scripts/train_hypnet.sh
+++ scripts/train_hypnet.sh
@@ -4,9 +4,11 @@
 #SBATCH --open-mode=append
 #SBATCH --ntasks=1
 #SBATCH --gres=gpu:1
-#SBATCH --partition=t4v1,t4v2,p100,rtx6000
+#SBATCH --partition=t4v1,t4v2,p100
 #SBATCH --cpus-per-gpu=1
 #SBATCH --mem=25GB
+#SBATCH --account=deadline
+#SBATCH --qos=deadline
 #SBATCH --exclude=gpu109
 
 python3 train_hypnet.py $1
