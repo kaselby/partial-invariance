30c9dd5c09a75d98a13a6d89a44604ded577382f
Kira Selby <kaselby@uwaterloo.ca>
2021-09-29 13:45:50 -0400

    fix wasserstein exact

--- utils.py
+++ utils.py
@@ -299,7 +299,7 @@ def wasserstein_exact(X, Y):
     bs = X.size(0)
     dists = torch.zeros(bs)
     for i in range(bs):
-        costs = ot.dist(X[i].cpu().numpy(), Y[i].cpu().numpy())
+        costs = ot.dist(X[i].cpu().numpy(), Y[i].cpu().numpy(), metric='euclidean')
         dists[i] = ot.emd2([],[],costs)
     if use_cuda:
         dists = dists.cuda()
