85f932d479e3cd807e39977c818ceee3a0dccf86
Kira Selby <kaselby@uwaterloo.ca>
2021-10-09 11:33:00 -0400

    test hypnet

--- train_hypnet.py
+++ train_hypnet.py
@@ -134,8 +134,9 @@ class HyponomyDataset(Dataset):
         dataset_path = os.path.join(data_dir, dataset_name + ".all")
         relations, pairs, labels = cls._read_dataset(dataset_path, min_threshold, inverted_pairs=inverted_pairs)
         n0 = len(pairs)
-        relations, pairs, labels = cls._trim_dataset(vecs, relations, pairs, labels, min_threshold)
-        print("Dataset contains %d pairs. %d pairs removed after filtering." % (n0, n0-len(pairs)))
+        if min_threshold >= 0:
+            relations, pairs, labels = cls._trim_dataset(vecs, relations, pairs, labels, min_threshold)
+            print("Dataset contains %d pairs. %d pairs removed after filtering." % (n0, n0-len(pairs)))
         return cls(vecs, relations, pairs, labels, pca_dim=pca_dim, max_vecs=max_vecs)
 
     def __init__(self, vecs, relations, pairs, labels, pca_dim=-1, max_vecs=-1):
