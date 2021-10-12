454918e3df93eafd213dd483080bb1999aed2491
Kira Selby <kaselby@uwaterloo.ca>
2021-10-12 15:35:09 -0400

    generating gmms

--- test_hypnet.py
+++ test_hypnet.py
@@ -50,7 +50,7 @@ def evaluate_fct(fct, dataset, batch_size=64):
 
     for i in tqdm.tqdm(range(len(dataset))):
         v1, v2, l = dataset[i]
-        v1, v2, l = torch.Tensor(v1), torch.Tensor(v2), torch.Tensor(l)
+        v1, v2, l = torch.Tensor(v1), torch.Tensor(v2), torch.BoolTensor([l])
         if use_cuda:
             v1, v2 = v1.cuda(), v2.cuda()
 
