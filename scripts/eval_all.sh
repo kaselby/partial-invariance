64775a9481bf61c0b581c643d3dc38fae301860e
Kira Selby <kaselby@uwaterloo.ca>
2021-10-21 10:55:29 -0400

    eval all

--- scripts/eval_all.sh
+++ scripts/eval_all.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DATASETS=("HypNet_test", "LenciBenotto", "EVALution", "BLESS", "BIBLESS", "Kotlerman2010", "Levy2014", "Turney2014", "Weeds")
+DATASETS=("HypNet_test" "LenciBenotto" "EVALution" "BLESS" "BIBLESS" "Kotlerman2010" "Levy2014" "Turney2014" "Weeds")
 
 for dataset in "${DATASETS[@]}"
 do
