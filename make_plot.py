9d6aaaabf2dfcf7982a2f6a662aec3082b37a3b4
Kira Selby <kaselby@uwaterloo.ca>
2021-10-28 10:21:19 -0400

    test

--- make_plot.py
+++ make_plot.py
@@ -10,16 +10,17 @@ import numpy as np
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('run_name', type=str)
+    parser.add_argument('run_names', type=str, nargs='+')
     args = parser.parse_args()
 
-    filename = os.path.join("runs", args.run_name, "logs.pt")
-    logs = torch.load(filename)
-    losses = logs['losses']
+    for run_name in args.run_names:
+        filename = os.path.join("runs", run_name, "logs.pt")
+        logs = torch.load(filename)
+        losses = logs['losses']
 
-    plt.plot(losses)
-    plt.legend()
-    plt.xlabel("Steps")
-    plt.ylabel("Mean Squared Error")
-    plt.yscale("log")
-    plt.show()
+        plt.plot(losses)
+        plt.legend()
+        plt.xlabel("Steps")
+        plt.ylabel("Mean Squared Error")
+        plt.yscale("log")
+        plt.show()
