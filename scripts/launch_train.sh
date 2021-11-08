1cd3bef18f003caa0af82d3b914608e72886f766
Kira Selby <kaselby@uwaterloo.ca>
2021-11-08 10:32:13 -0500

    lr

--- scripts/launch_train.sh
+++ scripts/launch_train.sh
@@ -13,7 +13,7 @@ run_name=$1
 
 for (( i = 0 ; i < $n_runs ; i++ ))
 do
-    sbatch scripts/train.sh "${run_name}/${i}" $target $data -1 0 $dim $(( dim*latent_size )) $(( dim*hidden_size )) "csab"
-    sbatch scripts/train.sh "${run_name}_equi/${i}" $target $data -1 1 $dim $latent_size $hidden_size "csab"
-    sbatch scripts/train.sh "${run_name}_pine/${i}" $target $data -1 0 $dim $(( dim*latent_size/2 )) $(( dim*hidden_size*2 )) "pine"
+    sbatch scripts/train.sh "${run_name}/${i}" $target $data -1 0 $dim $(( dim*latent_size )) $(( dim*hidden_size )) $lr "csab"
+    sbatch scripts/train.sh "${run_name}_equi/${i}" $target $data -1 1 $dim $latent_size $hidden_size $lr "csab" 
+    sbatch scripts/train.sh "${run_name}_pine/${i}" $target $data -1 0 $dim $(( dim*latent_size/2 )) $(( dim*hidden_size*2 )) $lr "pine"
 done
