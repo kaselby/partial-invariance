b94279305cf7dd6697abd4c101473951c7b6517a
Kira Selby <kaselby@uwaterloo.ca>
2021-11-09 13:22:23 -0500

    plots and such

--- test.py
+++ test.py
@@ -61,23 +61,24 @@ if __name__ == '__main__':
         generators={'corr':CorrelatedGaussianGenerator(return_params=exact_loss)}
         data_kwargs={'corr':{}}
 
+    basedir=os.path.join(RUN_DIR, args.target)
     for name,generator in generators.items():
         sample_kwargs = {**base_sample_kwargs, **data_kwargs[name]}
         print("%s:"%name)
         seed = torch.randint(100, (1,)).item()
         for run_name in args.run_names:
-            all_runs = get_runs(run_name, RUN_DIR)
+            all_runs = get_runs(run_name, basedir)
             if len(all_runs) > 0:
                 avg_loss=0
                 for run_num in all_runs:
-                    model = torch.load(os.path.join(RUN_DIR, args.target, run_name, run_num, "model.pt"))
+                    model = torch.load(os.path.join(basedir, run_name, run_num, "model.pt"))
                     model_loss = evaluate(model, generator, label_fct, 
                         sample_kwargs=sample_kwargs, steps=500, criterion=nn.L1Loss(), normalize=normalize, exact_loss=exact_loss, seed=seed)
                     avg_loss += model_loss
                     print("%s-%s Loss: %f" % (run_name, run_num, model_loss))
                 print("%s Avg Loss: %f" % (run_name, avg_loss / len(all_runs)))
             else:
-                model = torch.load(os.path.join(RUN_DIR, args.target, run_name, "model.pt"))
+                model = torch.load(os.path.join(basedir, run_name, "model.pt"))
                 model_loss = evaluate(model, generator, label_fct, 
                     sample_kwargs=sample_kwargs, steps=500, criterion=nn.L1Loss(), normalize=normalize, exact_loss=exact_loss, seed=seed)
                 print("%s Loss: %f" % (run_name, model_loss))
