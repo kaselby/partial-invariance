cdc78f89490a900359aa70e23b271f922ff88f80
Kira Selby <kaselby@uwaterloo.ca>
2021-11-09 14:54:57 -0500

    test

--- test_rho.py
+++ test_rho.py
@@ -25,10 +25,11 @@ if __name__ == '__main__':
     mi_true = mi_corr_gaussian(rho, d=args.n)
     mi_model = torch.zeros_like(rho)
     mi_kraskov = torch.zeros_like(rho)
-    for i in range(rho.size(0)):
-        X, T = generator(N, n=args.n, corr=rho[i])
-        mi_model[i] = model(*X).squeeze(-1).mean()
-        mi_kraskov[i] = kraskov_mi1(*X).mean()
+    with torch.no_grad():
+        for i in range(rho.size(0)):
+            X, T = generator(N, n=args.n, corr=rho[i])
+            mi_model[i] = model(*X).squeeze(-1).mean()
+            mi_kraskov[i] = kraskov_mi1(*X).mean()
 
     torch.save({'rho':rho.cpu(), 'true':mi_true.cpu(), 'model':mi_model.cpu(), 'kraskov':mi_kraskov.cpu()}, 
         os.path.join(args.basedir, "mi", args.run_name, "rho.pt"))
