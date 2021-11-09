18d20b1dc38a9384ce91dc0d1a02b37c684bab1d
Kira Selby <kaselby@uwaterloo.ca>
2021-11-09 13:53:04 -0500

    test clip

--- scripts/launch_train.sh
+++ scripts/launch_train.sh
@@ -9,12 +9,12 @@ latent_size=16
 hidden_size=32
 lr="1e-3"
 clip=-1
-
+basedir="final-runs"
 run_name=$1
 
 for (( i = 0 ; i < $n_runs ; i++ ))
 do
-    sbatch scripts/train.sh "${run_name}/${i}" $target $data -1 0 $dim $(( dim*latent_size )) $(( dim*hidden_size )) $lr $clip "csab" 
-    sbatch scripts/train.sh "${run_name}_equi/${i}" $target $data -1 1 $dim $latent_size $hidden_size $lr $clip "csab"
-    sbatch scripts/train.sh "${run_name}_pine/${i}" $target $data -1 0 $dim $(( dim*latent_size/2 )) $(( dim*hidden_size*2 )) $lr $clip "pine"
+    sbatch scripts/train.sh "${run_name}/${i}" $target $data -1 0 $dim $(( dim*latent_size )) $(( dim*hidden_size )) $lr $clip "csab" $basedir
+    sbatch scripts/train.sh "${run_name}_equi/${i}" $target $data -1 1 $dim $latent_size $hidden_size $lr $clip "csab" $basedir
+    sbatch scripts/train.sh "${run_name}_pine/${i}" $target $data -1 0 $dim $(( dim*latent_size/2 )) $(( dim*hidden_size*2 )) $lr $clip "pine" $basedir
 done
