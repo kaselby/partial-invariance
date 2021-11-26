2fece973444e7dadd97421675f4e54ca1e7c3ea2
Kira Selby <kaselby@uwaterloo.ca>
2021-11-26 12:31:45 -0500

    timing

--- time.py
+++ time.py
@@ -13,11 +13,12 @@ sizes = torch.linspace(2.5,8,10).exp().round().int()
 
 generator=GaussianGenerator(num_outputs=2)
 #model = torch.load(os.path.join("runs", run_name, "model.pt"))
-model1 = MultiSetTransformer(n, n*2, n*4, 1, equi=False, nn_attn=False).cuda()
-model2 = MultiSetTransformer(n, n*2, n*4, 1, equi=False, nn_attn=True).cuda()
-model3 = MultiSetTransformer(n, 16, 32, 1, equi=True, nn_attn=False).cuda()
-model4 = MultiSetTransformer(n, 16, 32, 1, equi=True, nn_attn=True).cuda()
-models = [model1, model2, model3, model4]
+models = {
+    'base': MultiSetTransformer(n, n*2, n*4, 1, equi=False, nn_attn=False).cuda(),
+    'nn_attn': MultiSetTransformer(n, n*2, n*4, 1, equi=False, nn_attn=True).cuda(),
+    'equi:': MultiSetTransformer(n, 16, 32, 1, equi=True, nn_attn=False).cuda(),
+    'nn_attn-equi': MultiSetTransformer(n, 16, 32, 1, equi=True, nn_attn=True).cuda()
+}
 baseline = kl_knn
 
 def test(generator, fct, bs=32, **kwargs):
@@ -28,17 +29,17 @@ results={}
 for s in sizes:
     size = s.item()
     #print("Size: ", size)
-    results[size]={'baseline':0, 'models':[]}
-    for model in models:
+    results[size]={}
+    for model_name, model in models.items():
         t_model = timeit.timeit(lambda: test(generator, model, n=n, set_size=(size, int(size*1.5))), number=N)
-        results[size]['models'].append(t_model)
+        results[size][model_name] = t_model
     t_baseline = timeit.timeit(lambda: test(generator, baseline, n=n, set_size=(size, int(size*1.5))), number=N)
     results[size]['baseline'] = t_baseline
 
-    print("%d:\n" % size)
-    print("\tbaseline: %f\n" % results[size]['baseline'])
-    for i,model in enumerate(models):
-        print("\model %d: %f\n" % (i, results[size]['models'][i]))
+    print("%d:" % size)
+    print("\tbaseline: %f" % results[size]['baseline'])
+    for model_name, model in models.items():
+        print("\t%s: %f" % (model_name, results[size][model_name]))
 
     
         
