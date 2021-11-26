17560c8782d667911ba62b15f322188cb60ef1bf
Kira Selby <kaselby@uwaterloo.ca>
2021-11-26 12:38:49 -0500

    timing

--- time.py
+++ time.py
@@ -7,7 +7,7 @@ import torch
 
 #run_name="w_24-40_gmm"
 
-n=16
+n=8
 N=300
 sizes = torch.linspace(2.5,7,8).exp().round().int()
 
@@ -16,8 +16,8 @@ generator=GaussianGenerator(num_outputs=2)
 models = {
     'base': MultiSetTransformer(n, n*2, n*4, 1, equi=False, nn_attn=False).cuda(),
     'nn_attn': MultiSetTransformer(n, n*2, n*4, 1, equi=False, nn_attn=True).cuda(),
-    'equi:': MultiSetTransformer(n, 16, 32, 1, equi=True, nn_attn=False).cuda(),
-    'nn_attn-equi': MultiSetTransformer(n, 16, 32, 1, equi=True, nn_attn=True).cuda()
+    'equi:': MultiSetTransformer(n, 8, 16, 1, equi=True, nn_attn=False).cuda(),
+    'nn_attn-equi': MultiSetTransformer(n, 8, 16, 1, equi=True, nn_attn=True).cuda()
 }
 baseline = kl_knn
 
