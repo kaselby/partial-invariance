136baaf9085e5c8ddfdc63dac91eb1447aefe5cd
Kira Selby <kaselby@uwaterloo.ca>
2021-11-30 14:53:57 -0500

    tqdm

---
+++ scripts/train_omniglot.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --job-name=test
+#SBATCH --output=logs/slurm-%j.txt
+#SBATCH --open-mode=append
+#SBATCH --ntasks=1
+#SBATCH --gres=gpu:2
+#SBATCH --partition=t4v1,t4v2,p100
+#SBATCH --cpus-per-gpu=1
+#SBATCH --mem=25GB
+#SBATCH --exclude=gpu109
+
+
+python3 train_omniglot.py $1 --checkpoint_name $SLURM_JOB_ID
