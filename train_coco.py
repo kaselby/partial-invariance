254785350a6bb172f2b5dda8d8811b0a439181e6
Kira Selby <kaselby@uwaterloo.ca>
2021-12-03 12:47:25 -0500

    coco

--- train_coco.py
+++ train_coco.py
@@ -18,7 +18,7 @@ from generators import ImageCooccurenceGenerator
 
 
 
-def load_caption_data(imgdir, anndir, tokenizer):
+def load_caption_data(imgdir, anndir):
     transforms = T.Compose([T.Resize(256), T.CenterCrop(224), T.ToTensor(), T.Normalize(mean=[0.485, 0.456, 0.406],
                                  std=[0.229, 0.224, 0.225])])
     
