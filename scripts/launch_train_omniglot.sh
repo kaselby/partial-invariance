2c44ac396da25ebf237a1fd9f7ea657e1cffe801
Kira Selby <kaselby@uwaterloo.ca>
2021-12-07 13:27:56 -0500

    merge eval

--- scripts/launch_train_omniglot.sh
+++ scripts/launch_train_omniglot.sh
@@ -8,7 +8,7 @@ n_runs=3
 
 for (( i = 0 ; i < $n_runs ; i++ ))
 do
-    sbatch scripts/train.sh "${run_name}_csab/${i}" $dataset $pretrain "csab"
-    sbatch scripts/train.sh "${run_name}_naive/${i}" $dataset $pretrain "naive"
-    sbatch scripts/train.sh "${run_name}_pine/${i}" $dataset $pretrain "pine"
+    sbatch scripts/train_omniglot.sh "${run_name}_csab/${i}" $dataset $pretrain "csab"
+    sbatch scripts/train_omniglot.sh "${run_name}_naive/${i}" $dataset $pretrain "naive"
+    sbatch scripts/train_omniglot.sh "${run_name}_pine/${i}" $dataset $pretrain "pine"
 done
