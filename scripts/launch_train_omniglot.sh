43bf8431fa403fbbbbbaae1a79c0a6ecb339657f
Kira Selby <kaselby@uwaterloo.ca>
2021-12-07 12:14:10 -0500

    launch train omni

---
+++ scripts/launch_train_omniglot.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+run_name=$1
+dataset=$2
+pretrain=$3
+
+n_runs=3
+
+for (( i = 0 ; i < $n_runs ; i++ ))
+do
+    sbatch scripts/train.sh "${run_name}_csab/${i}" $dataset $pretrain "csab"
+    sbatch scripts/train.sh "${run_name}_naive/${i}" $dataset $pretrain "naive"
+    sbatch scripts/train.sh "${run_name}_pine/${i}" $dataset $pretrain "pine"
+done
