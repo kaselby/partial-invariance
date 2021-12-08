f5b5217f2d50e71b41c3c88b93c5e16afe94c537
Kira Selby <kaselby@uwaterloo.ca>
2021-12-08 12:37:21 -0500

    fixes

--- scripts/train_coco.sh
+++ scripts/train_coco.sh
@@ -3,11 +3,11 @@
 #SBATCH --output=logs/slurm-%j.txt
 #SBATCH --open-mode=append
 #SBATCH --ntasks=1
-#SBATCH --gres=gpu:2
+#SBATCH --gres=gpu:4
 #SBATCH --partition=t4v1,t4v2,p100
 #SBATCH --cpus-per-gpu=1
 #SBATCH --mem=25GB
 #SBATCH --exclude=gpu109
 
 
-python3 train_coco.py $1 --checkpoint_name $SLURM_JOB_ID 
+python3 train_coco.py $1 --checkpoint_name $SLURM_JOB_ID --batch_size 16
