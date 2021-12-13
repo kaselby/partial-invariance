54db0a93b43769a2c503762f4933d6cdecb1c9b8
Kira Selby <kaselby@uwaterloo.ca>
2021-12-13 09:54:30 -0500

    fix coco

--- scripts/launch_train_omniglot.sh
+++ scripts/launch_train_omniglot.sh
@@ -3,12 +3,13 @@
 run_name=$1
 dataset=$2
 pretrain=$3
+lr=$4
 
 n_runs=3
 
 for (( i = 0 ; i < $n_runs ; i++ ))
 do
-    sbatch scripts/train_omniglot.sh "${run_name}_csab/${i}" $dataset $pretrain "csab"
-    sbatch scripts/train_omniglot.sh "${run_name}_naive/${i}" $dataset $pretrain "naive"
-    sbatch scripts/train_omniglot.sh "${run_name}_pine/${i}" $dataset $pretrain "pine"
+    sbatch scripts/train_omniglot.sh "${run_name}_csab/${i}" $dataset $pretrain "csab" $lr
+    sbatch scripts/train_omniglot.sh "${run_name}_naive/${i}" $dataset $pretrain "naive" $lr
+    sbatch scripts/train_omniglot.sh "${run_name}_pine/${i}" $dataset $pretrain "pine" $lr
 done
