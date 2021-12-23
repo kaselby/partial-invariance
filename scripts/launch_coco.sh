fa861c5cbca6d7e17fb6da256bdf03549aa3b1da
Kira Selby <kaselby@uwaterloo.ca>
2021-12-23 14:00:30 -0500

    launch coco

--- scripts/launch_coco.sh
+++ scripts/launch_coco.sh
@@ -13,7 +13,7 @@ steps=8000
 
 for (( i = 0 ; i < $n_runs ; i++ ))
 do
-    sbatch scripts/train.sh "${run_name}/${i}" $batch_size $latent_size $hidden_size $lr $num_blocks $steps "csab"
-    sbatch scripts/train.sh "${run_name}_naive/${i}" $batch_size $latent_size $hidden_size $lr $num_blocks $steps "naive"
-    sbatch scripts/train.sh "${run_name}_pine/${i}" $batch_size $latent_size $hidden_size $lr $num_blocks $steps "pine" 
+    sbatch scripts/train_coco.sh "${run_name}/${i}" $batch_size $latent_size $hidden_size $lr $num_blocks $steps "csab"
+    sbatch scripts/train_coco.sh "${run_name}_naive/${i}" $batch_size $latent_size $hidden_size $lr $num_blocks $steps "naive"
+    sbatch scripts/train_coco.sh "${run_name}_pine/${i}" $batch_size $latent_size $hidden_size $lr $num_blocks $steps "pine" 
 done
