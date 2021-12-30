172f5060e456113c068816184db920efbdf7ed54
Kira Selby <kaselby@uwaterloo.ca>
2021-12-29 20:04:05 -0500

    episodes

--- scripts/launch_coco.sh
+++ scripts/launch_coco.sh
@@ -2,18 +2,20 @@
 
 n_runs=3
 
-batch_size=8
+batch_size=6
 num_blocks=1
 latent_size=512
 hidden_size=1024
 lr="1e-5"
 basedir="final-runs"
 run_name=$1
-steps=8000
+steps=16000
+set_size1=3
+set_size2=10
 
 for (( i = 0 ; i < $n_runs ; i++ ))
 do
-    sbatch scripts/train_coco.sh "${run_name}/${i}" $batch_size $latent_size $hidden_size $lr $num_blocks $steps "csab"
-    sbatch scripts/train_coco.sh "${run_name}_naive/${i}" $batch_size $latent_size $hidden_size $lr $num_blocks $steps "naive"
-    sbatch scripts/train_coco.sh "${run_name}_pine/${i}" $batch_size $latent_size $hidden_size $lr $num_blocks $steps "pine" 
+    sbatch scripts/train_coco.sh "${run_name}/${i}" $batch_size $latent_size $hidden_size $lr $num_blocks $steps "csab" $set_size1 $set_size2
+    sbatch scripts/train_coco.sh "${run_name}_naive/${i}" $batch_size $latent_size $hidden_size $lr $num_blocks $steps "naive" $set_size1 $set_size2
+    sbatch scripts/train_coco.sh "${run_name}_pine/${i}" $batch_size $latent_size $hidden_size $lr $num_blocks $steps "pine" $set_size1 $set_size2
 done
