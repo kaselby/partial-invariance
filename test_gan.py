38eba22605977a18bb04fe2e973a719fd6117803
Kira Selby <kaselby@uwaterloo.ca>
2022-01-17 09:53:17 -0500

    test gan

--- test_gan.py
+++ test_gan.py
@@ -1,5 +1,6 @@
 import torch
 import os
+import tqdm
 
 from md_generator import MetaDatasetGenerator, Split
 
@@ -8,7 +9,7 @@ def eval_disc(model, episode, steps, batch_size, data_kwargs):
     N = batch_size * steps
     with torch.no_grad():
         y,yhat,dl,sd=[],[],[],[]
-        for i in range(steps):
+        for i in tqdm.tqdm(range(steps)):
             (X,Y), target, (dataset_level, same_dataset) = episode(batch_size, eval=True, **data_kwargs)
             out = model(X,Y).squeeze(-1)
             y.append(target)
@@ -60,9 +61,7 @@ def eval_by_dataset(model, episode, steps, batch_size, data_kwargs):
                 X, Y = episode.compare_datasets(i, j, batch_size=batch_size, **data_kwargs)
                 out = model(X,Y).squeeze(-1)
                 dist = -1 * F.logsigmoid(out)[0].mean()
-
-                label = int(i==j)
-                acc = ((out > 0) == label).mean()
+                acc = ((out > 0) == (i==j)).mean()
                 dists[i][j] += dist
                 accs[i][j] += acc
             dists[i][j] /= steps
@@ -82,6 +81,7 @@ image_size=84
 episode_classes=200
 episode_datasets=11
 steps=1000
+batch_size=16
 
 data_kwargs={
     'set_size':(10,30),
@@ -94,7 +94,7 @@ model = torch.load(os.path.join(basedir, dataset, run_name, "model.pt"))
 test_generator = MetaDatasetGenerator(image_size=image_size, split=Split.TEST, device=device)
 
 episode = test_generator.get_episode(episode_classes, episode_datasets)
-y,yhat, (dl, sd) = eval_disc(model, episode, steps, 16, data_kwargs)
+y,yhat, (dl, sd) = eval_disc(model, episode, steps, batch_size, data_kwargs)
 
 
 (acc, dl_acc, dl_pos_acc, dl_neg_acc, cl_acc, cl_pos_acc, cl_neg_acc, cl_neg_sd_acc, cl_neg_dd_acc), _ = summarize_eval(y, yhat, dl, sd, return_all=True)
@@ -103,3 +103,6 @@ y,yhat, (dl, sd) = eval_disc(model, episode, steps, 16, data_kwargs)
 #data_kwargs={
 #    'set_size':(6,10)
 #}
+#steps=16
+
+#dists,accs = eval_by_dataset(model, episode, steps, batch_size, data_kwargs)
