728bc68cbb70a530f42875dc7e5367cf8a4bec25
Kira Selby <kaselby@uwaterloo.ca>
2022-01-22 21:02:50 -0500

    Finish coco

--- scripts/finish_coco.sh
+++ scripts/finish_coco.sh
@@ -15,13 +15,13 @@ set_size2=10
 merge="concat"
 dataset="coco"
 
-models=("csab" "pine" "rn" "cross-only")
+models=("csab" "pine" "rn" "cross-only" "naive")
 
 for (( i = 0 ; i < $n_runs ; i++ ))
 do
     for model in "${models[@]}"
     do
-        if  [ ! -f "${basedir}/${dataset}/${model}/${i}" ]
+        if  [ ! -f "${basedir}/${dataset}/${run_name}_${model}/${i}" ]
         then
             sbatch scripts/train_coco.sh "${run_name}_${model}/${i}" $batch_size $latent_size $hidden_size $lr $num_blocks $steps $model $set_size1 $set_size2 $merge $dataset
         fi
