8d80650cd5858363cb3c9785ff5c17b1483b4b3c
Kira Selby <kaselby@uwaterloo.ca>
2022-01-31 15:43:08 -0500

    embed alignment

--- scripts/launch_gan.sh
+++ scripts/launch_gan.sh
@@ -8,12 +8,15 @@ data="md"
 n=8
 merge="concat"
 
+ls=512
+hs=1024
+
 for (( i = 0 ; i < $n_runs ; i++ ))
 do
-    sbatch scripts/train_gan.sh "${run_name}_csab/$i" "csab" $merge $data $n
-    sbatch scripts/train_gan.sh "${run_name}_pine/$i" "pine" $merge $data $n 
-    sbatch scripts/train_gan.sh "${run_name}_naive/$i" "naive" $merge $data $n
-    sbatch scripts/train_gan.sh "${run_name}_rn/$i" "rn" $merge $data $n
-    sbatch scripts/train_gan.sh "${run_name}_cross-only/$i" "cross-only" $merge $data $n
+    sbatch scripts/train_gan.sh "${run_name}_csab/$i" "csab" $merge $data $n $ls $hs
+    sbatch scripts/train_gan.sh "${run_name}_pine/$i" "pine" $merge $data $n $ls $hs
+    sbatch scripts/train_gan.sh "${run_name}_naive/$i" "naive" $merge $data $n $ls $hs
+    sbatch scripts/train_gan.sh "${run_name}_rn/$i" "rn" $merge $data $n $ls $hs
+    sbatch scripts/train_gan.sh "${run_name}_cross-only/$i" "cross-only" $merge $data $n $ls $hs
     #sbatch scripts/train_gan.sh "${run_name}_sum-merge/$i" "csab" "sum" $data $n
 done
