d91cb038cc67d38a64ad9b4eeb14b5f6d80fb357
Kira Selby <kaselby@uwaterloo.ca>
2022-01-31 15:43:58 -0500

    embed alignment

--- scripts/launch_gan.sh
+++ scripts/launch_gan.sh
@@ -8,15 +8,15 @@ data="md"
 n=8
 merge="concat"
 
-ls=512
-hs=1024
+latent_size=512
+hidden_size=1024
 
 for (( i = 0 ; i < $n_runs ; i++ ))
 do
-    sbatch scripts/train_gan.sh "${run_name}_csab/$i" "csab" $merge $data $n $ls $hs
-    sbatch scripts/train_gan.sh "${run_name}_pine/$i" "pine" $merge $data $n $ls $hs
-    sbatch scripts/train_gan.sh "${run_name}_naive/$i" "naive" $merge $data $n $ls $hs
-    sbatch scripts/train_gan.sh "${run_name}_rn/$i" "rn" $merge $data $n $ls $hs
-    sbatch scripts/train_gan.sh "${run_name}_cross-only/$i" "cross-only" $merge $data $n $ls $hs
+    sbatch scripts/train_gan.sh "${run_name}_csab/$i" "csab" $merge $data $n $latent_size $hidden_size
+    sbatch scripts/train_gan.sh "${run_name}_pine/$i" "pine" $merge $data $n $latent_size $hidden_size
+    sbatch scripts/train_gan.sh "${run_name}_naive/$i" "naive" $merge $data $n $latent_size $hidden_size
+    sbatch scripts/train_gan.sh "${run_name}_rn/$i" "rn" $merge $data $n $latent_size $hidden_size
+    sbatch scripts/train_gan.sh "${run_name}_cross-only/$i" "cross-only" $merge $data $n $latent_size $hidden_size
     #sbatch scripts/train_gan.sh "${run_name}_sum-merge/$i" "csab" "sum" $data $n
 done
