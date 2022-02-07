9023fb2c379ba34a1fc29160a5ae6673ea5f2217
Kira Selby <kaselby@uwaterloo.ca>
2022-02-07 10:30:37 -0500

    anneal ss

--- scripts/train_coco.sh
+++ scripts/train_coco.sh
@@ -15,6 +15,10 @@ if [ ${15} -eq 1 ]
 then
     argstring="${argstring} --ln"
 fi
+if [ ${16} -eq 1 ]
+then
+    argstring="${argstring} --anneal_set_size"
+fi
 
 
 python3 train_coco.py $1 --batch_size $2 --latent_size $3 --hidden_size $4 --lr $5 --num_blocks $6 --steps $7 --model $8 --set_size $9 ${10} --merge ${11} --dataset ${12} --warmup_steps ${13} --decoder_layers ${14} --checkpoint_name $SLURM_JOB_ID
