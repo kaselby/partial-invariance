ac6afcba759a22a03be3f210c677734a17ea7515
Kira Selby <kaselby@uwaterloo.ca>
2022-02-08 10:41:50 -0500

    hpsearch

--- scripts/launch_gan.sh
+++ scripts/launch_gan.sh
@@ -20,13 +20,14 @@ weight_sharing="none"
 ln=1
 num_blocks=1
 decoder_layers=1
+basedir="final-runs2"
 
 for (( i = 0 ; i < $n_runs ; i++ ))
 do
-    sbatch scripts/train_gan.sh "${run_name}_csab/$i" "csab" $merge $data $n $latent_size $hidden_size $bs $lr $steps $set_size1 $set_size2 $warmup_steps $weight_sharing $ln $num_blocks $decoder_layers
-    sbatch scripts/train_gan.sh "${run_name}_pine/$i" "pine" $merge $data $n $latent_size $hidden_size $bs $lr $steps $set_size1 $set_size2 $warmup_steps $weight_sharing $ln $num_blocks $decoder_layers
-    sbatch scripts/train_gan.sh "${run_name}_naive/$i" "naive" $merge $data $n $latent_size $hidden_size $bs $lr $steps $set_size1 $set_size2 $warmup_steps $weight_sharing $ln $num_blocks $decoder_layers
-    sbatch scripts/train_gan.sh "${run_name}_rn/$i" "rn" $merge $data $n $latent_size $hidden_size $bs $lr $steps $set_size1 $set_size2 $warmup_steps $weight_sharing $ln $num_blocks $decoder_layers
-    sbatch scripts/train_gan.sh "${run_name}_cross-only/$i" "cross-only" $merge $data $n $latent_size $hidden_size $bs $lr $steps $set_size1 $set_size2 $warmup_steps $weight_sharing $ln $num_blocks $decoder_layers
+    sbatch scripts/train_gan.sh "${run_name}_csab/$i" "csab" $merge $data $n $latent_size $hidden_size $bs $lr $steps $set_size1 $set_size2 $warmup_steps $weight_sharing $ln $num_blocks $decoder_layers $basedir
+    sbatch scripts/train_gan.sh "${run_name}_pine/$i" "pine" $merge $data $n $latent_size $hidden_size $bs $lr $steps $set_size1 $set_size2 $warmup_steps $weight_sharing $ln $num_blocks $decoder_layers $basedir
+    sbatch scripts/train_gan.sh "${run_name}_naive/$i" "naive" $merge $data $n $latent_size $hidden_size $bs $lr $steps $set_size1 $set_size2 $warmup_steps $weight_sharing $ln $num_blocks $decoder_layers $basedir
+    sbatch scripts/train_gan.sh "${run_name}_rn/$i" "rn" $merge $data $n $latent_size $hidden_size $bs $lr $steps $set_size1 $set_size2 $warmup_steps $weight_sharing $ln $num_blocks $decoder_layers $basedir
+    sbatch scripts/train_gan.sh "${run_name}_cross-only/$i" "cross-only" $merge $data $n $latent_size $hidden_size $bs $lr $steps $set_size1 $set_size2 $warmup_steps $weight_sharing $ln $num_blocks $decoder_layers $basedir
     #sbatch scripts/train_gan.sh "${run_name}_sum-merge/$i" "csab" "sum" $data $n
 done
