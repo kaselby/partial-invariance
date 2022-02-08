c74fccd0f77253de90e1141ddaa2843557620001
Kira Selby <kaselby@uwaterloo.ca>
2022-02-08 15:19:37 -0500

    merge

--- merge_eval.py
+++ merge_eval.py
@@ -7,19 +7,27 @@ def get_runs(run_name):
     subfolders = [f.name for f in os.scandir(run_name) if f.is_dir()]
     return subfolders
 
+
 model_suffixes = ['csab','pine','naive','rn','cross-only']
 
 parser = ArgumentParser()
 parser.add_argument('run_name', type=str)
 parser.add_argument('--basedir', type=str, default='final-runs')
 parser.add_argument('--dataset', type=str, default='mnist')
+parser.add_argument('--all', action='store_true')
 args = parser.parse_args()
 
 base_dir = os.path.join(args.basedir, args.dataset)
 
+if args.all:
+    run_paths = glob.glob(os.path.join(base_dir, args.run_name+"*"))
+    run_names = [run_path.split("/")[-1] for run_path in run_paths]
+else:
+    run_names=[args.run_name + "_" + model_name for model_name in model_suffixes]
+
 results={}
-for model_name in model_suffixes:
-    model_dir = os.path.join(base_dir, args.run_name + "_" + model_name)
+for run_name in run_names:
+    model_dir = os.path.join(base_dir, run_name)
     if os.path.exists(model_dir):
         runs = get_runs(model_dir)
         accs = []
@@ -29,7 +37,7 @@ for model_name in model_suffixes:
                 logs = torch.load(run_dir)
                 accs.append(logs['test_acc'])
         if len(accs) > 0:
-            results[model_name] = {'all_accs': accs, 'avg_acc': sum(accs)/len(accs)}
+            results[run_name] = {'all_accs': accs, 'avg_acc': sum(accs)/len(accs)}
 
 output_file = os.path.join(base_dir, args.run_name +"_results.txt")
 with open(output_file, 'w') as outfile:
