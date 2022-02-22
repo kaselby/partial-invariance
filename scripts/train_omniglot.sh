eb8ff5958a9635be726dfc56f5026434095c2235
Kira Selby <kaselby@uwaterloo.ca>
2022-02-22 18:23:21 -0500

    fix coco

--- scripts/train_omniglot.sh
+++ scripts/train_omniglot.sh
@@ -15,4 +15,4 @@ then
     argstring="${argstring} --ln"
 fi
 
-python3 train_omniglot.py $1 --checkpoint_name $SLURM_JOB_ID --dataset $2 --pretrain_steps $3 --lr $4 --model $5 --weight_sharing $6 --merge $7 --warmup_steps $8 --latent_size $9 --hidden_size ${10} --num_blocks ${11} --batch_size ${12} --steps ${13} --set_size ${14} ${15} --dropout ${16} $argstring
+python3 train_omniglot.py $1 --checkpoint_name $SLURM_JOB_ID --dataset $2 --pretrain_steps $3 --lr $4 --model $5 --weight_sharing $6 --merge $7 --warmup_steps $8 --latent_size $9 --hidden_size ${10} --num_blocks ${11} --batch_size ${12} --steps ${13} --set_size ${14} ${15} --dropout ${16} --ss_schedule ${18} $argstring
