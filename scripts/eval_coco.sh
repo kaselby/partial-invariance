fe6052b9fd9d50af4a0f0ddb148009d73bb2c1ee
Kira Selby <kaselby@uwaterloo.ca>
2022-05-31 14:26:48 -0400

    stuf

--- scripts/eval_coco.sh
+++ scripts/eval_coco.sh
@@ -3,10 +3,10 @@
 #SBATCH --output=logs/slurm-%j.txt
 #SBATCH --open-mode=append
 #SBATCH --ntasks=1
-#SBATCH --gres=gpu:1
+#SBATCH --gres=gpu:2
 #SBATCH --partition=t4v1,t4v2,p100
 #SBATCH --cpus-per-gpu=1
 #SBATCH --mem=25GB
 #SBATCH --exclude=gpu109
 
-python3 test_coco.py $1 --outfile $2 --set_size $2 $3 --eval_all
+python3 test_coco.py $1 --set_size $2 $3 --eval_all
