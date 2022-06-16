b2bc5a512ac7c96a2f0f7f055f5c6c83bdf1000f
Kira Selby <kaselby@uwaterloo.ca>
2022-06-16 13:44:03 -0400

    device

--- tasks.py
+++ tasks.py
@@ -1,6 +1,6 @@
 
 from builders import SET_MODEL_BUILDERS, CONV_MODEL_BUILDERS
-from trainer import Trainer, CountingTrainer, MetaDatasetTrainer, StatisticalDistanceTrainer, Pretrainer
+from trainer import Trainer, CountingTrainer, CaptionTrainer, MetaDatasetTrainer, StatisticalDistanceTrainer, Pretrainer
 from datasets.counting import OmniglotCooccurenceGenerator, ImageCooccurenceGenerator, DatasetByClass, load_cifar, load_mnist, load_omniglot
 from datasets.alignment import EmbeddingAlignmentGenerator, CaptionGenerator, load_coco_data, load_flickr_data, bert_tokenize_batch, fasttext_tokenize_batch
 from datasets.distinguishability import DistinguishabilityGenerator
