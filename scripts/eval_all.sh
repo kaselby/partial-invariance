fc9d46b69e10736f18493f3b98be083071822adb
Kira Selby <kaselby@uwaterloo.ca>
2022-06-28 12:31:57 -0400

    clean up

--- scripts/eval_all.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-DATASETS=("HypNet_test" "LenciBenotto" "EVALution" "BLESS" "BIBLESS" "Kotlerman2010" "Levy2014" "Turney2014" "Weeds")
-
-for dataset in "${DATASETS[@]}"
-do
-    sbatch scripts/eval_hypnet.sh $1 $dataset
-done
